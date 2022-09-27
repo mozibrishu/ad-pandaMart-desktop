a16c7bec40cd9e6807b09107f9d82ec287d813be
Mozib <mozib.diu@gmail.com>
2022-09-27 11:50:43 +0600

    animation time changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-gsap.timeline()
+gsap.timeline({defaults:{ease: "power2.out"}})
     .to(".element_2", { duration: 1,y: 2,repeat:-1,yoyo: true })
-    .to(".element_6", { duration: 1,scaleX: 1.01,scaleY:1.01,repeat:-1,yoyo: true },"<")
-    .fromTo(".element_5", {rotate:-1},{ duration: 1,rotate:1,repeat:-1,yoyo: true },"<")
+    .fromTo(".element_5", {rotate:-1},{ duration: 2,rotate:1,repeat:-1,yoyo: true },"<")
+    .to(".element_6", { duration: 1,scaleY: 1.05,repeat:-1,yoyo: true },"<.2")
